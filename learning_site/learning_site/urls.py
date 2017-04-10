51f71e045f4badaa1a1c53462d72dc136eb54c1a
tracylm <tracy.murray@yahoo.com>
2017-04-10 16:05:45 -0400

    Added courses

--- learning_site/learning_site/urls.py
+++ learning_site/learning_site/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
+    url(r'^courses/', include('courses.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.hello_world),
 ]
