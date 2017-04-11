413f6ef6abf4ff9f60ba82f6c484577bf77fe827
tracylm <tracy.murray@yahoo.com>
2017-04-11 18:43:27 -0400

    Added Step detail view and Content field:

--- learning_site/courses/models.py
+++ learning_site/courses/models.py
@@ -12,8 +12,12 @@ class Course(models.Model):
 class Step(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
+    content = models.TextField(blank=True, default='')
     order = models.IntegerField(default=0)
     course = models.ForeignKey(Course)
+
+    class Meta:
+        ordering = ['order',]
 
     def __str__(self):
         return self.title
--- learning_site/courses/views.py
+++ learning_site/courses/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
 
-from . models import Course
+
+from . models import Course, Step
+
+def hello_world(request):
+    return HttpResponse('Hello World')
 
 def course_list(request):
     courses = Course.objects.all()
     return render(request, 'courses/course_list.html', {'courses': courses})
 
+
 def course_detail(request, pk):
-    course = Course.objects.get(pk=pk)
+    course = get_object_or_404(Course, pk=pk)
     return render(request, 'courses/course_detail.html', {'course': course})
+
+
+def step_detail(request, course_pk, step_pk):
+    step = get_object_or_404(Step, course_id=course_pk, pk=step_pk)
+    return render(request, 'courses/step_detail.html', {'step': step})
